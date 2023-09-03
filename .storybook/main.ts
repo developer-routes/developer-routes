f291392c5f5beac2669e4ca0e5cfab0dc61586ad
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-09-03 20:16:05 +0530

    chore: developer routes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-     "@storybook/addon-controls",
+    "@storybook/addon-controls",
   ],
   framework: {
     name: "@storybook/nextjs",
---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+    <div>about</div>
+  )
+}
+
+export default About
