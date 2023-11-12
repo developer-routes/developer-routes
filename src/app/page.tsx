b7ab3329353b68c0be3448d643f341b7a029b554
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-11-12 21:25:41 +0530

    chore: add new page content and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex p-3 h-screen">
-      ABC
+      <div className='flex flex-col mt-10 items-center space-y-5 w-full'>
+          <h1 className='text-7xl font-semibold leading-tight'>Community for Connection, <br></br> Creativity & Collaboration</h1>
+          <p>Made by and for creatives who believe in the power of ideas and the joy of making them happen</p>
+          <button className='border-2 rounded-full p-4'>Join our community</button>
+      </div>
     </main>
   )
 }
