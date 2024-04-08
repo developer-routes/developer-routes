0c7d4e1d5a9d28298f9905cf0c772cd4d079dc16
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2024-04-08 10:57:32 +0530

    jest configuration

---
+++ __test__/index.test.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+      render(<Footer/>)
+  })
+})
--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,17 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const route = useRouter();
   return (
-    <div className="flex flex-col mt-10 items-center space-y-5 w-full h-screen ">
-      <h1 className="text-7xl font-semibold leading-tight">
-        Community for Connection, <br></br> Creativity & Collaboration
+    <div className="flex flex-col mt-10 items-center space-y-5 w-full h-screen  p-[100px]">
+      <h1 className="text-7xl font-semibold text-center">
+      Empower Developers with Powerful Tools to Build Amazing Things
       </h1>
       <p>
-        Made by and for creatives who believe in the power of ideas and the joy
-        of making them happen
+      Boost your developer workflow and accelerate innovation with our comprehensive suite of tools.
       </p>
       <button onClick={()=>{
         route.push('https://discord.gg/9s2SanmY')
       }} className="border-2 rounded-full p-4 hover:bg-blue-950">
-        Join our community
+        View All Our Products
       </button>
     </div>
   );
